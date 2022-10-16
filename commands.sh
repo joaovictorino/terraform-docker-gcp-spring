fdd0334b9892e6d450c860d031128d5793577823
joaovictorino <jhvictorino@gmail.com>
2022-10-16 00:00:22 -0300

    iaas

---
+++ commands.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install Terraform
+# Create GCP account
+# Create Service Account
+# Install gcloud cli
+
+gcloud init
+gcloud compute images list
---
+++ vm/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+}
+
+resource "google_compute_network" "vpc-network" {
+  name = "vpc-network"
+}
+
+resource "google_compute_instance" "vm-instance" {
+  name         = "vm-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc-network.name
+    access_config {}
+  }
+}
+
+output "ip" {
+  value = google_compute_instance.vm-instance.network_interface.0.access_config.0.nat_ip
+}
