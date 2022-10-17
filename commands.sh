c9eaae483209bd5b1b75635f1441ea4964d42e81
joaovictorino <jhvictorino@gmail.com>
2022-10-16 23:10:43 -0300

    java application

--- commands.sh
+++ commands.sh
@@ -5,11 +5,22 @@
 # Create Service Account
 # Install gcloud cli
 
+# Login and initialize cli
 gcloud init
 gcloud auth application-default login
 
+# Create project
 gcloud projects create "palestra-ICI" --name="paletra-ICI"
 
+# List VM images
 gcloud compute images list
 
+# Login Docker GCP
+gcloud auth configure-docker
+
+# Tag image GCP repository
+docker tag springapp:latest gcr.io/palestra-ici/springapp:latest
+
+# Upload image
+docker push gcr.io/palestra-ici/springapp:latest
 
--- main.tf
+++ main.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "google" {
-  project     = var.project
-  region      = var.region
-  zone        = var.zone
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_cloud_run_service" "run-service" {
@@ -20,7 +20,22 @@ resource "google_cloud_run_service" "run-service" {
   template {
     spec {
       containers {
-        image = "gcr.io/google-samples/hello-app:1.0"
+        image = "gcr.io/palestra-ici/springapp:latest"
+
+        env {
+          name = "MYSQL_URL"
+          value = "jdbc:mysql://tflab-mysqlserver-1-teste.mysql.database.azure.com:3306/exampledb?useSSL=true&requireSSL=false"
+        }
+        
+        env {
+          name = "MYSQL_USER"
+          value = "root"
+        }
+        
+        env {
+          name = "MYSQL_PASS"
+          value = "Teste@admin123"
+        }
       }
     }
   }
@@ -36,6 +51,21 @@ resource "google_cloud_run_service_iam_member" "run-service-all-members" {
   location = google_cloud_run_service.run-service.location
   role     = "roles/run.invoker"
   member   = "allUsers"
+}
+
+resource "google_sql_database_instance" "db-service" {
+  name             = "db-service"
+  region           = var.region
+  database_version = "MYSQL_5_7"
+  root_password = "Teste@admin123"
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+resource "google_container_registry" "container-registry" {
+  project  = var.project
+  location = "US"
 }
 
 output "service-url" {
